4effe332852f0df481247ac828754c6152f4a971
Levelfourtwenty <level420@protonmail.com>
2022-06-27 06:32:56 +0100

    Added backend multipart uploading and support for sharex uploads.

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
-from flask import Flask, flash, request, redirect, render_template
+from flask import Flask, flash, request, redirect, jsonify, render_template
 import boto3
 import os
 from werkzeug.utils import secure_filename
 import random
 import string
 
+safe_urls = ['uploads.trihard.space', 'trollpepe.com', 'trolling.solutions', 'holocaust.today', 'africans.shop', 'shitpost.domains']
 app = Flask(__name__)
 upload_folder = './'
 app.config['UPLOAD_FOLDER'] = upload_folder
@@ -13,6 +14,12 @@ aws_access = os.environ["s3_access"]
 aws_secret = os.environ["s3_secret"]
 bucket = os.environ["s3_bucket"]
 s3 = boto3.client('s3', endpoint_url="https://s3.wasabisys.com", aws_access_key_id=aws_access, aws_secret_access_key=aws_secret)
+transfer = boto3.s3.transfer
+config = transfer.TransferConfig(multipart_threshold=1024 * 15,
+                       max_concurrency=10,
+                        multipart_chunksize=1024 * 25,
+                        use_threads=True)
+
 
 def blacklist_file(filename):
 	# checks if file is an html file to prevent xss attacks
@@ -37,11 +44,14 @@ def namegen():
 @app.route("/", methods=['POST'])
 def upload():
 	file = request.files['file']
-	if '.' not in file.filename:
-		return render_template('error.html', error='Illegal file extension')
+	url = request.form['Upload Site']
+	if url not in safe_urls:
+		return render_template('error.html', error='Invalid upload site.')
 	if 'file' not in request.files:
 		# check if file part is in request
 		return render_template('error.html', error='File not in request')
+	if '.' not in file.filename:
+		return render_template('error.html', error='Illegal file extension')
 	if file.filename == '':
 		# checks if file is selected
 		return render_template('error.html', error='File not selected')
@@ -52,10 +62,43 @@ def upload():
 		#randomizes and temporarily saves file, will be deleted after upload to s3 bucket
 		print(filename)
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		s3.upload_file(f'./{filename}', bucket, filename)
+		s3.upload_file(f'./{filename}', bucket, filename, Config=config)
 		os.remove(f"./{filename}")
-		return render_template("success.html", filename=filename)
+		return render_template("success.html", filename=filename, site=url)
 	if blacklist_file(file.filename) == 1:
 		return render_template('error.html', error='Illegal file extension')
 
-app.run(host="127.0.0.1", port=8888)
+
+# Uncomment the below code if you wish to host the uploader.html file using flask 
+
+@app.route("/uploader", methods=['GET', 'POST'])
+def render():
+ 	return render_template('uploader.html')
+
+
+@app.route("/sharex", methods=['POST'])
+def sharex():
+	file = request.files['file']
+	url = request.form['url']
+	if url not in safe_urls:
+		return jsonify(error="Invalid upload site")
+	if 'file' not in request.files:
+		# check if file part is in request
+		return jsonify(error="File not included in request")
+	if file.filename == '':
+		# checks if file is selected
+		return jsonify(error="Filename is blank")
+	if blacklist_file(file.filename) == 0:
+		filext = file.filename.rsplit('.', 1)[1].lower()
+		filename = namegen()
+		filename = f"{filename}.{filext}"
+		#randomizes and temporarily saves file, will be deleted after upload to s3 bucket
+		print(filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		s3.upload_file(f'./{filename}', bucket, filename, Config=config)
+		os.remove(f"./{filename}")
+		return jsonify(url=f"https://{url}/{filename}")
+	if blacklist_file(file.filename) == 1:
+		return jsonify(error="Illegal file type")
+
+app.run(host="0.0.0.0", port=8888)
